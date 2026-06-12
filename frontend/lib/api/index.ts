7c0ad6b3b8cc0b67807911f7d9614fd104fc1d0f
Duncanmaina58 <dun.can.duntez@gmail.com>
2026-06-13 01:41:33 +0300

    Switch frontend to alpine, enable BuildKit - reduce image size for stable push

--- frontend/lib/api/index.ts
+++ frontend/lib/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8082';
 
 const api = axios.create({
   baseURL: API_URL,
